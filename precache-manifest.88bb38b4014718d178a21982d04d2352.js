1df7d702421000831d9565499e1d77d53e6813ee
Alexander Mischuk <mischuk.alexander@gmail.com>
2020-09-16 11:49:38 +0300

    Updates

---
+++ precache-manifest.88bb38b4014718d178a21982d04d2352.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf3112c86d1869b83a66e3fb05573fd",
+    "url": "/rnd/index.html"
+  },
+  {
+    "revision": "1f12bb383b12fb16e01b",
+    "url": "/rnd/static/css/2.fd29c4ed.chunk.css"
+  },
+  {
+    "revision": "5bcce6db25879e7ba13a",
+    "url": "/rnd/static/css/main.777d9a77.chunk.css"
+  },
+  {
+    "revision": "1f12bb383b12fb16e01b",
+    "url": "/rnd/static/js/2.5a8f6cb2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rnd/static/js/2.5a8f6cb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bcce6db25879e7ba13a",
+    "url": "/rnd/static/js/main.21ab8c6f.chunk.js"
+  },
+  {
+    "revision": "08652b2f569cd793b0ec",
+    "url": "/rnd/static/js/runtime-main.617fb56b.js"
+  }
+]);
